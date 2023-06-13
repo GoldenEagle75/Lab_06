210948da7ebb13b7198580d440cc1e0ba308f3b2
GoldenEagle75 <marshall.muhlhauser@gmail.com>
2023-06-13 17:46:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+class CtoFConverter {
+    public static void main(String[] args) {
+    }
+}
+class FuelCosts {
+    public static void main(String[] args){
+
+    }
+}
+
+class RectangleInfo {
+    public static void main(String[] args){
+
+    }
+}
+
+class MetricConverter {
+    public static void main(String[] args){
+
+    }
+}
+
+class HighorLow {
+    public static void main(String[] args){
+
+    }
+}
