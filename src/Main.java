15b4aa0c8b2ca3248673ec4763c433fd69023239
GoldenEagle75 <marshall.muhlhauser@gmail.com>
2023-06-14 14:07:59 -0400

    Task 2 Input Completed

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,63 @@ class CtoFConverter {
 }
 class FuelCosts {
     public static void main(String[] args){
-        System.out.println("Fuel Costs");
+        double tankGallons = 0;
+        double fuelEfficiency = 0;
+        double gasPrice = 0;
+        double costForHundred = 0;
+        double maxDistance = 0;
+        boolean done = false;
+        String trash = "";
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("How many gallons are in the tank? ");
+            if (in.hasNextDouble()){
+                tankGallons = in.nextDouble();
+                done = true;
+                System.out.println("You said you have " + tankGallons + " gallons in the tank.");
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("Enter a valid value, not " + trash + ".");
+                System.out.println("Try again.");
+            }
+        }while (!done);
+
+        done = false;
+        in.nextLine();
+
+        do {
+            System.out.print("What is the fuel efficiency in miles per gallon? ");
+            if (in.hasNextDouble()){
+                fuelEfficiency = in.nextDouble();
+                done = true;
+                System.out.println("You said that you have a fuel efficiency of " + fuelEfficiency + " miles per gallon.");
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("Enter a valid value, not " + trash + ".");
+                System.out.println("Try again.");
+            }
+        }while (!done);
+
+        done = false;
+        in.nextLine();
+
+        do {
+            System.out.print("What is the price of gas per gallon? ");
+            if (in.hasNextDouble()){
+                gasPrice = in.nextDouble();
+                done = true;
+                System.out.println("You said the gas price is $" + gasPrice + " per gallon.");
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("Enter a valid value, not " + trash + ".");
+                System.out.println("Try again.");
+            }
+        }while (!done);
+
+        //costForHundred = 100 * gasPrice;
     }
 }
 
