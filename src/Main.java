1d268ee4d9970ec265e54b0f06e91afda2d2b808
GoldenEagle75 <marshall.muhlhauser@gmail.com>
2023-06-15 12:11:18 -0400

    Task 3 Completed

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ public class Main {
         FuelCosts.main(args);
         RectangleInfo.main(args);
         MetricConverter.main(args);
-        HighorLow.main(args);
+        HighOrLow.main(args);
     }
 }
 
 class CtoFConverter {
     public static void main(String[] args) {
-        double CelsiusTemp = 0;
-        double FahrenheitTemp = 0;
+        double CelsiusTemp;
+        double FahrenheitTemp;
         boolean done = false;
-        String trash = "";
+        String trash;
         Scanner in = new Scanner(System.in);
         do {
             System.out.print("Enter the temperature you wish to convert from Celsius to Fahrenheit: ");
@@ -37,10 +37,10 @@ class FuelCosts {
         double tankGallons = 0;
         double fuelEfficiency = 0;
         double gasPrice = 0;
-        double costForHundred = 0;
-        double maxDistance = 0;
+        double costForHundred;
+        double maxDistance;
         boolean done = false;
-        String trash = "";
+        String trash;
         Scanner in = new Scanner(System.in);
         do {
             System.out.print("How many gallons are in the tank? ");
@@ -99,7 +99,54 @@ class FuelCosts {
 
 class RectangleInfo {
     public static void main(String[] args){
-        System.out.println("rectangle info");
+
+        double rectangleHeight = 0;
+        double rectangleWidth = 0;
+        String trash;
+        double rectangleArea;
+        double rectanglePerimeter;
+        double rectangleDiagonal;
+        boolean done = false;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter the width: ");
+            if (in.hasNextDouble()){
+                rectangleWidth = in.nextDouble();
+                done = true;
+                System.out.println("You said the width was " + rectangleWidth + ".");
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("Enter a valid value, not " + trash + ".");
+                System.out.println("Try again.");
+            }
+        }while (!done);
+
+        done = false;
+        in.nextLine();
+
+        do {
+            System.out.print("Enter the height: ");
+            if (in.hasNextDouble()){
+                rectangleHeight = in.nextDouble();
+                done = true;
+                System.out.println("You said the height was " + rectangleHeight + ".");
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("Enter a valid value, not " + trash + ".");
+                System.out.println("Try again.");
+            }
+        }while (!done);
+
+        rectanglePerimeter = (rectangleWidth*2) + (rectangleHeight*2);
+        rectangleArea = rectangleWidth*rectangleHeight;
+        rectangleDiagonal = Math.sqrt((Math.pow(rectangleWidth, 2) + Math.pow(rectangleHeight, 2)));
+
+        System.out.println("The perimeter is " + rectanglePerimeter + ".");
+        System.out.println("The area is " + rectangleArea + ".");
+        System.out.printf("The length of the diagonal is %.2f.%n", rectangleDiagonal);
     }
 }
 
@@ -109,7 +156,7 @@ class MetricConverter {
     }
 }
 
-class HighorLow {
+class HighOrLow {
     public static void main(String[] args){
         System.out.println("High or low");
     }
